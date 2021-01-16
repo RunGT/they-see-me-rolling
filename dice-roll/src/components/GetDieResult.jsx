856dfd456a447e22329b33a40fbd5dadb90f6456
Glenroy-Terence (RunGT) <glenroyterencedev@gmail.com>
2021-01-16 18:27:08 +0000

    Create a get request to the server and log JSON data

--- dice-roll/src/components/GetDieResult.jsx
+++ dice-roll/src/components/GetDieResult.jsx
@@ -4,19 +4,28 @@ import "../App.css";
 import d1 from "../assets/images/d1.png";
 
 export default function GetDieResult() {
+  const [dieRollResult, setDieRollResult] = useState(0);
 
-    const [dieRollResult, setDieRollResult] = useState(0);
+  // Error handling: If there isn't any response from the server return an error
 
-    const checkResponse = (response) => {
-        if (!response.ok) throw new Error(`Network error: ${response.status}`);
-        return response.json();
-    }
+  const checkResponse = (response) => {
+    if (!response.ok) throw new Error(`Network error: ${response.status}`);
+    return response.json();
+  };
 
+  const rollDie = () => {
+    fetch("https://rolz.org/api/?d6.json")
+      .then(checkResponse)
+      .then((result) => {
+        console.log(result);
+      });
+  };
 
-    
+  rollDie();
+
   return (
     <main>
-      <img src={d1} alt="A die displaying 1" className="die"/>
+      <img src={d1} alt="A die displaying 1" className="die" />
     </main>
   );
 }
