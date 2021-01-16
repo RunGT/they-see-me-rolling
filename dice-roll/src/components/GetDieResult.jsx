62cdf63c6969ba931ef8f79ce152a3cd4fd516d8
Glenroy-Terence (RunGT) <glenroyterencedev@gmail.com>
2021-01-16 18:19:14 +0000

    Create state for die roll result and create a function for error handling

--- dice-roll/src/components/GetDieResult.jsx
+++ dice-roll/src/components/GetDieResult.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../App.css";
 import d1 from "../assets/images/d1.png";
 
 export default function GetDieResult() {
+
+    const [dieRollResult, setDieRollResult] = useState(0);
+
+    const checkResponse = (response) => {
+        if (!response.ok) throw new Error(`Network error: ${response.status}`);
+        return response.json();
+    }
+
+
+    
   return (
     <main>
       <img src={d1} alt="A die displaying 1" className="die"/>
